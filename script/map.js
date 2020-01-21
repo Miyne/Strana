310c5c011d3bfc4e41c5fe241684bac9feef9f55
Miyne <57369769+Miyne@users.noreply.github.com>
2020-01-21 19:31:59 +0100

    Add files via upload

--- script/map.js
+++ script/map.js
@@ -1,3 +1,42 @@
+//confondre menu.html et  map.html
+
+document.addEventListener("DOMContentLoaded", function(e) {
+
+    // recuperer param dans URL
+    switch (window.location.href.substr(window.location.href.indexOf('?'))) {
+        case 'nuke':
+                //
+                break;
+        case 'overpass':
+                //
+                break;
+        case 'dust2':
+                //
+                break;
+        case 'mirage':
+                //
+                break;
+        case 'inferno':
+                //
+                break;
+        case 'vertigo':
+                //
+                break;
+        case 'cache':
+                //
+                break;
+        case 'train':
+                //
+                break;
+        default:
+                //Error
+    }
+
+    // récupère l'element main
+
+    // rempli le main avec le contenu que tu veux
+
+})
 
 function createPage(){
     document.getElementById("root").innerHTML=`
@@ -9,12 +48,34 @@ function createPage(){
     </button>`
 }
 
+function displayStrana(){
+    document.getElementByID("root").innerHTML=`
+    <aside id="left">
+        <select name="strat">
+            <option value="test1">TEST strat1</option>
+            <option value="test2">TEST strat2</option>
+            <option value="test3">TEST strat3</option>
+            <option value="test4">TEST strat4</option>
+            <option value="test5">TEST strat5</option>
+        </select>
+    </aside>
+
+    <aside id="right">
+        <select name="stuff">
+            <option value="test1">TEST stuff1</option>
+            <option value="test2">TEST stuff2</option>
+            <option value="test3">TEST stuff3</option>
+            <option value="test4">TEST stuff4</option>
+            <option value="test5">TEST stuff5</option>
+        </select>
+    </aside>`
+}
+
 function onButtonClick(choice) {
     console.log("You just clicked: " + choice);
     document.getElementById("ct").style = "display: none";
     document.getElementById("t").style = "display: none";
+    displayStrana();
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    createPage();
- }, false);
+document.addEventListener('DOMContentLoaded', function() {createPage();}, false);
